28ffacc94ae34cee634aa32c3e865b2a2796bced
muslima <nematovamuslima53@gmail.com>
2025-02-24 17:24:38 +0500

    first added

--- app_users/models.py
+++ app_users/models.py
@@ -50,14 +50,6 @@ class User(AbstractBaseUser, PermissionsMixin):
     def has_module_perms(self, app_label):
         return self.is_admin
 
-
-class User2(models.Model):
-    email = models.EmailField(unique=True)
-
-    def __str__(self):
-        return self.email
-
-
 class TokenModel(models.Model):
     date = models.DateField()
     token = models.TextField()
