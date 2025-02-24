da95b184f19376a9d37b8fb02372522628c0869a
muslima <nematovamuslima53@gmail.com>
2025-02-24 17:53:58 +0500

    first added

--- app_movies/permissions.py
+++ app_movies/permissions.py
@@ -1,7 +1,7 @@
-from rest_framework.permissions import BasePermission,SAFE_METHODS
+from rest_framework import permissions
 
-class IsAdminOrReadOnly(BasePermission):
+class IsAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
-        if request.method in SAFE_METHODS:
+        if request.method in permissions.SAFE_METHODS:
             return True
-        return request.user.is_authenticated and request.user.is_admin or request.user.is_staff
+        return request.user and request.user.is_staff
